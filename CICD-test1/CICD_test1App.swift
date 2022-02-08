fe2e94dde3b5fe2669eca2a014a4ffa0e03b4df8
Uzir <uzirthapa@gmail.com>
2022-02-08 07:30:34 -0600

    Initial Commit

---
+++ CICD-test1/CICD_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_test1App.swift
+//  CICD-test1
+//
+//  Created by Uzir Thapa on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CICD_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
